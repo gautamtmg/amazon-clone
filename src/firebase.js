dcfbc64b830d18d16c4ad56f9e3617ca568cb1a1
Gautam Tamang <gautam.tamang0000@gmail.com>
2021-07-06 19:16:38 +0545

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChNy9opDVXs_kWNlUh2PGD8ThIVABdK5Y",
+    authDomain: "e-clone-3d4a2.firebaseapp.com",
+    projectId: "e-clone-3d4a2",
+    storageBucket: "e-clone-3d4a2.appspot.com",
+    messagingSenderId: "986770289703",
+    appId: "1:986770289703:web:9f665f347769a3a5acca02",
+    measurementId: "G-P7TS7JXN25"
+  };
